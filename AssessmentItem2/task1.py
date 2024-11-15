576ef25d765709dea9af723c036f622612a27bd8
Charles <35784256+notxcharles@users.noreply.github.com>
2024-11-15 14:48:04 +0000

    Remove and show duplicates
    Return a list of unique elements - this is done by casting the list into a set, and then casting the set back to a list. Sets can only contain unique elements
    
    List of duplicates - iterate through the original list and add each number to a dictionary. We keep track of the frequency of each element in the list. After, we can iterate through the dictionary, every key with a value higher than 1 can be added to a list of duplicates.

--- AssessmentItem2/task1.py
+++ AssessmentItem2/task1.py
@@ -1,7 +1,6 @@
 def AskUserForPositiveInteger():
     while True:
         userInput = input("Enter a positive integer (or done) ")
-        
         if (userInput.lower() == "done"):
             return 0
         
@@ -13,7 +12,32 @@ def AskUserForPositiveInteger():
                     
         if (userInputInteger > 0):
             return userInputInteger
+        
         print("Input was incorrect")
+
+
+def GetUniqueIntegers(integerList: list[int]) -> list[int]:
+    # Sets only contain unique elements
+    integerSet = set(integerList)
+    integerList = list(integerSet)    
+    return integerList
+
+
+def GetDuplicateIntegers(originalElements: list[int], uniqueElements: list[str]) -> list[str]:
+    integerQuantity = {}
+    for integer in originalElements:
+        if (integer not in integerQuantity):
+            integerQuantity[integer] = 0
+            continue
+        integerQuantity[integer] += 1
+        
+    duplicateIntegers = []
+    for integer in integerQuantity:
+        if (integerQuantity[integer] != 1):
+            duplicateIntegers.append(integer)
+            
+    return duplicateIntegers
+        
 
 def main():
     integerList = []
@@ -23,5 +47,11 @@ def main():
             # user wants to see stats
             break
         integerList.append(userInteger)
-            
+        
+    uniqueList = GetUniqueIntegers(integerList)
+    duplicateIntegers = GetDuplicateIntegers(integerList, uniqueList)
+    
+    print(f"{duplicateIntegers=} | {integerList=} | {uniqueList=}")
+    
+    
 main()
